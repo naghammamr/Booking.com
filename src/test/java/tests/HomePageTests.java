9a4c937b6e57af71e10ad292a24dd187b022cd99
Nagham Amr <naghamamr@gmail.com>
2022-12-13 14:12:29 +0200

    add excel reader

--- src/test/java/tests/HomePageTests.java
+++ src/test/java/tests/HomePageTests.java
@@ -1,15 +1,27 @@
 package tests;
 
+import data.ExcelReader;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pages.HomePage;
+
+import java.io.IOException;
 
 public class HomePageTests extends BaseTests {
 
     HomePage homePage;
 
-    @Test
-    public void xxx() {
+    @DataProvider(name="ExcelData")
+    public Object[][] userRegisterData() throws IOException
+    {
+        ExcelReader ER = new ExcelReader();
+        return ER.getDataFromExcel();
+    }
+
+    @Test(dataProvider="ExcelData")
+    public void xxx(String location, String checkin, String checkout) {
         homePage = new HomePage(driver);
+
     }
 
 }
